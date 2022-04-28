fd9620f1a80f4c3e5321c0146248a60a6b3dd3bf
kushites <sanjaykushites@gmail.com>
2022-04-28 22:59:59 +0530

    what

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'), // change this
+    publicPath: '/',
+    filename: 'bundle.js'
   },
+  devServer: {
+    contentBase: "./build",
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ['babel-loader', 'eslint-loader']
+    },
+    {
+      test: /\.less$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'less-loader',
+      ],
+    },
+   ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('./index.html'),
+    }),
+  ]
 };
